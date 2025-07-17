17a063c6c98cb42a02551de5207ae8d2be0ee13e
komkarsinga <komkarsinga@gmail.com>
2025-07-17 10:50:57 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# bokepceweksmp
-bokepceweksmp
+# [bokepceweksmp](https://bokepceweksmp.pages.dev)
---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Bokep Cewek Smp';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Adik Montok Smp Mama Live Viral Colmek Sma Hijab Tante China';
+export const url = 'https://bokepceweksmp.pages.dev';
+export const terbit = '2025-01-13T00:11:23Z';
+
+export const google = '7sfwZbmaGXo1rhTD9ktq_tWC2bKvTMbjriuzyp7rLyc';
+export const yandex = 'cb8331758d97a217'
+
+export const index = 'f8333ec4-afb4-4943-b9c7-f589a6ab887a';
